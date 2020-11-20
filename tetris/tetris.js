ccce1937f3c1fa00136e2fed003223e3115313d0
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-11-21 01:11:00 +0900

    game 사이트

---
+++ tetris/tetris.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//selectors
+const cvs = document.getElementById('tetris');
+const ctx = cvs.getContext('2d');
+const row = 20;
+
+//Canvas Drawing
+function drawSquare(x,y,color) {
+
+}
