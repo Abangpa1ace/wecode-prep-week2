7ada9e5928666f72bdd8098c1fb9ea885e116645
TH a.k.a Abangpa1ace <67219914+Abangpa1ace@users.noreply.github.com>
2020-11-22 15:24:04 +0900

    Update updown.js

--- updown/updown.js
+++ updown/updown.js
@@ -2,25 +2,61 @@
 
 const number = document.querySelector('.number');
 const msg = document.querySelector('.msg');
+const refresh = document.getElementById('refresh');
+const refreshAnswer = document.querySelector('.refresh-answer');
+const refreshBtn = document.querySelector('.refresh-btn');
+
+let answerNum = getRandomNum();
+// console.log(answerNum);
+function getRandomNum() {
+  return Math.floor(Math.random() * 100) + 1;
+}
 
 //Web speech API
 if ('speechSynthesis' in window) {
   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   const recognition = new SpeechRecognition();
+  recognition.lang = 'ko-KR'
+  // recognition.continuous = true;
 
-  recognition.onstart = () => {
-    console.log('recording...')
+  function speechRecognition() {
+    recognition.onstart = () => {
+      // console.log('recording')
+      recognition.onresult = (event) => {
+        let transcript = event.results[0][0].transcript;
+        let calledNum = parseInt(transcript, 10);
+        recognition.stop();
+        
+        number.innerText = calledNum;
+        if (isNaN(calledNum)) {
+          msg.innerText = 'Please speak a number.'
+        }
+        else {
+          if (calledNum > 100 || calledNum < 1) {
+            msg.innerText = 'Number must be between 1 and 100.'
+          }
+          else if (calledNum < answerNum) {
+            msg.innerText = 'Go Upper!'
+          }
+          else if (calledNum > answerNum) {
+            msg.innerText = 'Go Lower!'
+          }
+          else if (calledNum === answerNum) {
+            msg.innerText = 'Correct!'
+            refresh.style.display = 'flex';
+            refreshAnswer.innerText = calledNum;
+            refreshBtn.addEventListener('click', () => {
+              location.reload();
+            })
+          }
+        }
+        // speech recognition continuously
+        window.requestAnimationFrame(speechRecognition);
+      }
+    };
+
+    recognition.start();
   }
-
-  recognition.onspeechend = () => {
-    recognition.stop();
-  }
-
-  recognition.onresult = (event) => {
-    let transcript = event.results[0][0].transcript;
-  };
-
-  recognition.start();
-  
-} else {alert('Speech recognition is not provided.😅')}
-
+  speechRecognition();
+} 
+else {alert('Speech recognition is not provided.😅')}
